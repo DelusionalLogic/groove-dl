e65c92248308d3f6554b0ea1a48ff1a635e313c1
Jesper Jensen <DelusionalLogic@gmail.com>
2012-06-19 17:13:27 +0200

    Added a library
    Recoded the existsing groove.py to be more like a library.
    The library contains everything you should need to get started.
    
    The old wget download method has been booted in favor of an integrated
    python downloader that supports callBacks on every blocks downloaded
    (mainly for status updates to the user)
    
    The functions has been rewritten to reuse as much code as possible,
    while still keeping them simple.
    
    Everything is the library is kept public, as the script still gets
    banned, and the user might want to code his own procedure to download.
    
    Cookies (session ID's) are now saved on exit, and reused if they are not
    expired, possibly making grooveshark happier. (this add a new
    dependency, cookielib, should be included with python)
    
    QueueID's are now generated correctly
    
    I'm using urlgrabber.keepalive to support HTTP/1.1 and keep-alive, this
    is not included in the newer versions, so it requires an old version of
    urlgrabber.
    
    All prints have been removed, that is now left to the end user.

---
+++ python/groovylib/example.py
@@ -0,0 +1,18 @@
+import groovylib
+
+nh = groovylib.groovylib()
+
+s = nh.getResultsFromSearch(raw_input("Search Term: "))
+i = 0
+for item in s:
+    i = i + 1
+    print item["ArtistName"] + " - " + item["SongName"]
+    if i == 10:
+        break
+
+nh.download(s, raw_input("Please Select: "), reporter)
+
+def reporter(downloaded, size):
+    status = r"%10d  [%3.2f%%]" % (downloaded, downloaded * 100. / size)
+    status = status + chr(8)*(len(status)+1)
+    print status
---
+++ python/groovylib/groovylib.py
@@ -0,0 +1,261 @@
+import urllib2
+import json
+import string
+import hashlib
+import uuid
+import os
+import cookielib
+import gzip
+import StringIO
+import random
+import re
+import math
+import time
+import downloadHandler
+from urlgrabber.keepalive import HTTPHandler
+
+class groovylib:
+    '''Handles all the network stuff'''
+
+    downloader = downloadHandler.downloadHandler()
+
+    URL = "https://grooveshark.com"
+    USERAGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0"
+    COOKIEFILE = "groovy.cookie"
+
+    jsQueue = {}
+    jsQueue["client"] = "jsqueue"
+    jsQueue["clientRevision"] = "20120312.08"
+    jsQueue["secret"] = "circlesAndSquares"
+    jsQueue["headers"] = {
+                         "User-Agent":USERAGENT,
+                         "Host":"grooveshark.com",
+                         "Accept-Encoding":"gzip, deflate",
+                         "Content-Type":"application/json",
+                         "Accept-Language":"da,en-us;q=0.7,en;q=0.3"
+                         }
+
+    htmlshark = {}
+    htmlshark["client"] = "htmlshark"
+    htmlshark["clientRevision"] = "20120312"
+    htmlshark["secret"] = "reallyHotSauce"
+    htmlshark["headers"] = {
+                         "User-Agent":USERAGENT,
+                         "Host":"grooveshark.com",
+                         "Accept-Encoding":"gzip, deflate",
+                         "Content-Type":"application/json",
+                         "Accept-Language":"da,en-us;q=0.7,en;q=0.3"
+                         }
+
+    #Setting the static header (Country, session and uuid)
+    h = {}
+    h["country"] = {}
+    h["country"]["CC1"] = 72057594037927940
+    h["country"]["CC2"] = 0
+    h["country"]["CC3"] = 0
+    h["country"]["CC4"] = 0
+    h["country"]["ID"] = 57
+    h["country"]["IPR"] = 0
+    h["privacy"] = 0
+    h["uuid"] = str.upper(str(uuid.uuid4()))
+
+    token = None
+    session = None
+    userTrackingID = None
+    queueID = None
+    cj = None
+
+    def __init__(self):
+        self.installHandlers()
+        self.getSession()
+        self.getToken()
+        self.generateQueueID()
+
+    def installHandlers(self):
+        global cj
+        #Install support for KeepAlive and HTTP/1.1, via urlgrabber (older versions) 
+        keepalive_handler = HTTPHandler()
+        opener = urllib2.build_opener(keepalive_handler)
+        urllib2.install_opener(opener)
+
+        #Install cookielib for easy cookie management
+        self.cj = cookielib.LWPCookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        urllib2.install_opener(opener)
+
+        #Proxy stuff, used for testing, might be added at a later date
+        #proxy = urllib2.ProxyHandler({"https":"119.252.160.34:8000"})
+        #opener = urllib2.build_opener(proxy)
+        #urllib2.install_opener(opener)
+
+    def parseMainPage(self, html):
+        global userTrackingID
+        matchObj = re.search(r'"userTrackingID":[^}]*', html, re.I|re.M)
+        if matchObj:
+            matchObj = re.search(r"[0123456789]+", matchObj.group(), re.I|re.M)
+            if matchObj:
+                self.userTrackingID = matchObj.group()
+                return
+        raise SyntaxError("TrackingID not found")
+
+    def readSession(self):
+        for cookie in self.cj:
+            if cookie.name == "PHPSESSID":
+                session = cookie.value
+                return session
+        return
+
+    def getSession(self):
+        global session
+
+        if os.path.isfile(self.COOKIEFILE):
+            self.cj.load(self.COOKIEFILE)
+        req = urllib2.Request(self.URL)
+        page = urllib2.urlopen(req)
+        self.parseMainPage(page.read())
+        self.session = self.readSession()
+        self.cj.save(self.COOKIEFILE)
+
+    def createHeader(self, data, client, method = None):
+        data["header"] = self.h
+        data["header"]["session"] = self.session
+        data["header"]["client"] = client["client"]
+        data["header"]["clientRevision"] = client["clientRevision"]
+        if method:
+            data["header"]["token"] = self.generateToken(method, client["secret"])
+            data["method"] = method
+        return data
+
+    def createRequest(self, data, client):
+        return urllib2.Request(self.URL + "/more.php?" + data["method"], json.JSONEncoder().encode(data), client["headers"])
+
+    def generateQueueID(self):
+        global queueID
+        part1 = self.userTrackingID + str(math.floor(time.time()))
+        part2 = str(math.floor(random.random() * 500))
+        while len(part2)<3:
+            part2 = "0" + part2
+        self.queueID = part1 + part2
+
+    def generateToken(self, methodName, secret):
+        rnd = (''.join(random.choice(string.hexdigits) for x in range(6))).lower()
+        return rnd + hashlib.sha1('%s:%s:%s:%s' % (methodName, self.token, secret, rnd)).hexdigest()
+
+    def getToken(self):
+        global token
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["secretKey"] = hashlib.md5(self.session).hexdigest()
+        p["method"] = "getCommunicationToken"
+        p = self.createHeader(p, self.htmlshark)
+        print p
+        page = urllib2.urlopen(self.createRequest(p, self.htmlshark))
+        self.token = json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def getResultsFromSearch(self, query, what="Songs"):
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["type"] = what
+        p["parameters"]["query"] = query
+        p = self.createHeader(p, self.htmlshark, "getResultsFromSearch")
+        page = urllib2.urlopen(self.createRequest(p, self.htmlshark))
+        j = json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())
+        try:
+            return j["result"]["result"]["Songs"]
+        except:
+            return j["result"]["result"]
+
+    def getStreamKeyFromSongIDEx(self, id):
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["type"] = 0
+        p["parameters"]["mobile"] = False
+        p["parameters"]["prefetch"] = False
+        p["parameters"]["songID"] = id
+        p["parameters"]["country"] = self.h["country"]
+        p = self.createHeader(p, self.jsQueue, "getStreamKeyFromSongIDEx")
+        page = urllib2.urlopen(self.createRequest(p, self.jsQueue))
+        return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def addSongsToQueue(self, songObj, source = "user"):    
+        queueObj = {}
+        queueObj["songID"] = songObj["SongID"]
+        queueObj["artistID"] = songObj["ArtistID"]
+        queueObj["source"] = source
+        queueObj["songQueueSongID"] = 1
+    
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["songIDsArtistIDs"] = []
+        p["parameters"]["songIDsArtistIDs"].append(queueObj)
+        p["parameters"]["songQueueID"] = self.queueID
+        p = self.createHeader(p, self.jsQueue, "addSongsToQueue")
+        page = urllib2.urlopen(self.createRequest(p, self.jsQueue))
+        return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def removeSongsFromQueue(self, userRemoved = True):
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["songQueueID"] = self.queueID
+        p["parameters"]["userRemoved"] = True
+        p["parameters"]["songQueueSongIDs"]=[1]
+        p = self.createHeader(p, self.jsQueue, "removeSongsFromQueue")
+        page = urllib2.urlopen(self.createRequest(p, self.jsQueue))
+        return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def markSongDownloadedEx(self, streamServer, songID, streamKey):
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["streamServerID"] = streamServer
+        p["parameters"]["songID"] = songID
+        p["parameters"]["streamKey"] = streamKey
+        p = self.createHeader(p, self.jsQueue, "markSongDownloadedEx")
+        page = urllib2.urlopen(self.createRequest(p, self.jsQueue))
+        return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def markSongQueueSongPlayed(self, streamServerID, streamKey, songID):
+        p = {}
+        p["parameters"] = {}
+        p["parameters"]["streamServerID"] = streamServerID
+        p["parameters"]["streamKey"] = streamKey
+        p["parameters"]["songQueueSongID"] = 1
+        p["parameters"]["songQueueID"] = self.queueID
+        p["parameters"]["songID"] = songID
+        p = self.createHeader(p, self.jsQueue, "markSongQueueSongPlayed")
+        page = urllib2.urlopen(self.createRequest(p, self.jsQueue))
+        return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(page.read()))).read())["result"]
+
+    def download(self, search, choice, callBack = None):
+        try:
+            songid = int(choice)
+        except:
+            raise SyntaxError("Failed to convert choice to int")
+
+        
+        self.addSongsToQueue(search[songid]) #Add the song to the queue
+        
+        stream = self.getStreamKeyFromSongIDEx(search[songid]["SongID"]) #Get the StreamKey for the selected song
+        if stream == None:
+            raise Exception("StreamKey not found")
+
+        #markTimer = threading.Timer(30 + random.randint(0,5), self.markStreamKeyOver30Seconds, [search[choice]["SongID"], str(self.queueID), stream["ip"], stream["streamKey"]])
+        #markTimer.start()
+        print stream
+        data = {"streamKey":stream["streamKey"]}
+        headers = {"Accept-Encoding":"gzip, deflate", "Host":stream["ip"], "User-Agent":self.USERAGENT}
+        try:
+            
+
+            self.markSongDownloadedEx(stream["ip"], search[songid]["SongID"], stream["streamKey"])
+
+            self.downloader.download(("http://%s/stream.php" % (stream["ip"])), ("%s - %s.mp3" % (search[songid]["ArtistName"], search[songid]["SongName"])), data, headers, callBack)
+            
+            self.markSongQueueSongPlayed(stream["ip"], stream["streamKey"], search[songid]["SongID"])
+
+            self.removeSongsFromQueue()
+
+        except KeyboardInterrupt:
+
+            os.remove('%s - %s.mp3' % (search[songid]["ArtistName"], search[songid]["SongName"]))
+
+            self.markSongDownloadedEx(stream["ip"], search[songid]["SongID"], stream["streamKey"])
